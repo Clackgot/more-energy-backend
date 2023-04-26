f571872ec417b1052b848f796200f60e8281f0a9
clackgot <clackgot@gmail.com>
2023-04-26 14:30:32 +0300

    * Добавлен контроллер вывода всех пользователей * Добавлен контроллер создания нового пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Inject, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/createUser.dto';
 @Controller('users')
 export class UsersController {
+    constructor(@Inject(UsersService) private readonly usersService : UsersService){}
     @Get()
-    getUser() {
-        return "users";
+    getUsers() {
+        return this.usersService.getUsers();
+    }
+    @Post()
+    createUser(@Body() dto: CreateUserDto) {
+        return this.usersService.createUser(dto);
     }
 }
