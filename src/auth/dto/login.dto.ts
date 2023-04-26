cb3b85af28b4777ff29c3cb67cb917d1fd4ad768
clackgot <clackgot@gmail.com>
2023-04-26 17:16:17 +0300

    * Добавлена документация для dto входа и регистрации

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Trim } from 'class-sanitizer';
 import { IsEmail, IsString } from 'class-validator';
 
-
 export class LoginDto {
-    @Trim()
-    @IsEmail()
-    public readonly email: string;
+  @ApiProperty({
+    description: 'Адрес электронной почты пользователя',
+    example: 'user@example.com',
+  })
+  @Trim()
+  @IsEmail()
+  public readonly email: string;
 
-    @IsString()
-    public readonly password: string;
+  @ApiProperty({
+    description: 'Пароль пользователя',
+    example: 'password123',
+  })
+  @IsString()
+  public readonly password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,17 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Trim } from 'class-sanitizer';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
-
 export class RegisterDto {
-    @Trim()
-    @IsEmail()
-    public readonly email: string;
+  @ApiProperty({
+    description: 'Адрес электронной почты пользователя',
+    example: 'user@example.com',
+  })
+  @Trim()
+  @IsEmail()
+  public readonly email: string;
 
-    @IsString()
-    @MinLength(8)
-    public readonly password: string;
+  @ApiProperty({
+    description: 'Пароль пользователя. Длина должна быть не менее 8 символов',
+    example: 'password123',
+  })
+  @IsString()
+  @MinLength(8)
+  public readonly password: string;
 
-    @IsString()
-    @IsOptional()
-    public readonly username?: string;
+  @ApiProperty({
+    description: 'Имя пользователя',
+    example: 'John Doe',
+  })
+  @IsString()
+  @IsOptional()
+  public readonly username: string;
 }
