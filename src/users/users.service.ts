69a34896563a396ef1a9ef60eb2fcf94d7ff80cb
clackgot <clackgot@gmail.com>
2023-04-26 14:30:57 +0300

    * Рализован UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUser.dto';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User)
+    private readonly usersRepository: Repository<User>,) { }
+
+
+    async getUsers(){
+        return await this.usersRepository.find();
+    }
+    async createUser(dto: CreateUserDto) : Promise<User | never> {
+        return await this.usersRepository.save(dto);
+    }
+}
