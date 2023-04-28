0d7481cf57409a4dcb8365a82439cb65cc4c055c
clackgot <clackgot@gmail.com>
2023-04-28 16:34:25 +0300

    Добавлен корс

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-    .setTitle('My API')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addTag('my-api')
-    .build();
+  .setTitle('My API')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .addTag('my-api')
+  .build();
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  };
+  app.enableCors(corsOptions);
+
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+  await app.listen(PORT, () => console.log(`Серевер запущен на ${PORT} порту`))
 
 }
 start()
