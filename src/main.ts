bd0fbb199f3eb48cc042d3e825079ccaf9a036aa
clackgot <clackgot@gmail.com>
2023-04-28 20:35:42 +0300

    удалены неиспользуемые .env файлы

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 
 async function start() {
-  const PORT = process.env.HTTP_PORT || 5000;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .setTitle('My API')
-  .setDescription('API description')
-  .setVersion('1.0')
-  .addTag('my-api')
-  .build();
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
-  };
-  app.enableCors(corsOptions);
-
-
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('my-api')
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
