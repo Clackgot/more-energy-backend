7b3a1a643ba331583cf11629dd9cc2e4151d6b6b
clackgot <clackgot@gmail.com>
2023-05-04 11:40:03 +0300

    * Обновлен пример .env * Добавлен вывод в консоль текущего домена

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.API_SERVER_PORT || 5000;
+  const API_SERVER_DOMAIN = process.env.API_SERVER_DOMAIN;
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
     .setTitle('My API')
@@ -15,7 +16,7 @@ async function start() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+  await app.listen(PORT, () => console.log(`Сервер запущен на ${API_SERVER_DOMAIN}:${PORT} порту`))
 
 }
 start()
